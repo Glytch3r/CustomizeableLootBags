38cc0a434db5546e1b29a774044f14fb36f2fd49
Glytch3r <glytch3r.arts@gmail.com>
2025-01-30 18:23:57 +0800

    init

---
+++ media/lua/client/CustomizeableLootBags_Core.lua
@@ -0,0 +1,262 @@
+--[[██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                        				 Custom  PZ  Mod  Developer  for  Hire													  |
+|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|
+|                       	Portfolio:  https://steamcommunity.com/id/glytch3r/myworkshopfiles/							          |
+|                       		                                    														 	  |
+|                       	Discord:    Glytch3r#1337 / glytch3r															      |
+|                       		                                    														 	  |
+|                       	Support:    https://ko-fi.com/glytch3r														    	  |
+|_______________________________________________________________________________________________________________________________-]]
+--[[_____________________________________________________________________________________________________________________________
+   ░▒▓██████▓▒░    ░▒▓████████▓▒░    ░▒▓█▓▒░         ░▒▓█▓▒░      ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░           ░▒▓█▓▒░         ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▓▒░  ░▒▓▒░
+  ░▒▓█▓▒▒▓███▓▒░   ░▒▓█▓▒░         ░▒▓██████▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█████████▓▒░     ░▒▓███▓▒░     ░▒▓███████▓▒░
+  ░▒▓█▓▒░          ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░         ░▒▓█▓▒░ ░▒▓█▓▒░         ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+  ░▒▓█▓▒░░▒▓█▓▒░   ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░     ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░ ░▒▓█▓▒░  ▒▓░    ░▒▓█▓▒░   ░▒▓█▒░  ░▒▓█▒░
+   ░▒▓█████▓▒░     ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓███████▓▒░   ░▒▓██████▓▒░   ░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓███████▓▒░    ░▒▓███████▓▒░
+█████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████--]]
+
+CustomizeableLootBags = CustomizeableLootBags or {}
+
+--[[
+CustomizeableLootBags.colList = {
+	[1]="Blue",
+	[2]="Gray",
+	[3]="Green",
+	[4]="Pink",
+	[5]="Red",
+	[6]="White",
+	[7]="Yellow",
+}
+ ]]
+--local ico = 'Item_MysteryLoot_'..colList[int]
+-----------------------            ---------------------------
+function CustomizeableLootBags.doRoll()
+	local percent = SandboxVars.CustomizeableLootBags.ItemSpawnChance or 50
+	if percent == 0 then return false end
+	if percent == 100 then return true end
+	return percent >= ZombRand(1, 101)
+end
+
+function  CustomizeableLootBags.isBag(item)
+	return instanceof(item, "InventoryItem") and (instanceof(item, "InventoryContainer") or item:getCategory() == "Container")
+end
+-----------------------            ---------------------------
+function  CustomizeableLootBags.getStrList(item)
+    local cont = item:getInventory()
+    local itemCounts = {}
+
+    for i = 0, cont:getItems():size() - 1 do
+        local curItem = cont:getItems():get(i)
+        local fType = curItem:getFullType()
+        itemCounts[fType] = (itemCounts[fType] or 0) + 1
+    end
+
+    local strList = {}
+    for fType, qty in pairs(itemCounts) do
+        table.insert(strList, string.format("%s:%d", fType, qty))
+    end
+
+    return table.concat(strList, ";")
+end
+-----------------------
+--[[
+function CustomizeableLootBags.getIconStr(item)
+	local isVanilla = item:isVanilla()
+	local texture = item:getTexture()
+	local textureName = texture:getName()
+ 	if textureName:find("[/\\]") then
+		textureName = textureName:match("([^/\\]+)%.png$")
+	else
+		textureName = textureName:match("([^/\\]+)")
+	end
+	return texture:getName()
+end
+ ]]
+function CustomizeableLootBags.getIconStr(item)
+    local isVanilla = item:isVanilla()
+    local texture = item:getTexture()
+    local textureName = texture:getName()
+    if isVanilla then
+        return textureName
+    else
+        return "media/textures/" .. textureName .. ".png"
+    end
+end
+
+
+
+function CustomizeableLootBags.setIcon(item, ico)
+	item:setTexture(getTexture(ico))
+end
+
+-----------------------
+function CustomizeableLootBags.setLootBag(bag, shouldRemove)
+    if CustomizeableLootBags.isBag(bag) then
+		local showTip = SandboxVars.CustomizeableLootBags.ShowContentsTooltip or true
+        local lootbag = "Base.MysteryLootBag"
+        local pl = getPlayer()
+        local inv = pl:getInventory()
+        local fType = bag:getFullType()
+        local toSpawn = inv:AddItem(lootbag)
+		local ico = CustomizeableLootBags.getIconStr(bag)
+        toSpawn:setName("Loot Bag")
+        toSpawn:getModData()['LootBagData'] = {}
+        local strList = tostring(CustomizeableLootBags.getStrList(bag))
+        toSpawn:getModData()['LootBagData'].strList =  strList
+        toSpawn:getModData()['LootBagData'].fType =  fType
+        toSpawn:getModData()['LootBagData'].ico = tostring(ico)
+        if showTip then
+			toSpawn:setTooltip("Spawns: "..strList)
+		end
+        if shouldRemove then inv:DoRemoveItem(bag) end
+    else
+        print("Err: Invalid Item")
+    end
+end
+
+
+-----------------------            ---------------------------
+function  CustomizeableLootBags.isLootBag(ref)
+    local bool = false
+    if ref:hasModData() then
+        if ref:getModData()['LootBagData'] ~= nil then
+            bool = true
+        end
+    end
+    return bool
+end
+-----------------------            ---------------------------
+function  CustomizeableLootBags.spawnStrList(bag, strList)
+	local pl = getPlayer()
+	local inv = pl:getInventory()
+	local count = 0
+	local limit = SandboxVars.CustomizeableLootBags.MaxItemSpawn or 5
+	if instanceof(bag, "InventoryItem") then
+		if CustomizeableLootBags.isBag(bag) then
+			for var in string.gmatch(strList, "([^;]+)") do
+				local fType, qty = var:match("([^:]+):(%d+)")
+				if fType and qty then
+					if getScriptManager():FindItem(fType) then
+						for i = 1, tonumber(qty) do
+							if count >= limit then return end
+							if CustomizeableLootBags.doRoll() then
+								bag:getInventory():AddItem(fType)
+								count = count + 1
+							end
+						end
+					else
+						print("Err: No Such Item ", tostring(fType))
+					end
+				end
+			end
+		else
+			print("Err: "..tostring(bag:getFullType()).." is not an InventoryContainer")
+		end
+	else
+		print("Err: Invalid Item")
+	end
+end
+
+
+-----------------------            ---------------------------
+function  CustomizeableLootBags.openLootBag(ref, shouldRemove)
+    if CustomizeableLootBags.isLootBag(ref) then
+        local pl = getPlayer()
+        local inv = pl:getInventory()
+
+        local strList = tostring(ref:getModData()['LootBagData'].strList)
+        local fType = tostring(ref:getModData()['LootBagData'].fType)
+		local ico = tostring(ref:getModData()['LootBagData'].ico)
+		local name = tostring(ref:getName())
+        local bag = nil
+        local itemType = fType:match("^.+%.(.+)$") or fType
+
+        if getScriptManager():FindItem(fType) then
+
+
+            CustomizeableLootBags.spawnStrList(bag, strList)
+			local scr = ScriptManager.instance:getItem(fType)
+			scr:DoParam("WorldStaticModel = "..tostring(itemType))
+
+			bag = inv:AddItem(fType)
+			bag:setName(name)
+
+        else
+            print("Err: No Such Item ", tostring(fType))
+        end
+
+        if shouldRemove then
+            inv:DoRemoveItem(ref)
+        end
+	else
+		print("Err: Missing LootBagData")
+    end
+end
+
+
+-----------------------            ---------------------------
+
+-----------------------            ---------------------------
+function CustomizeableLootBags.invContext(player, context, items)
+    local pl = getSpecificPlayer(player)
+	for i, check in ipairs(items) do
+		local item = nil
+		local opts = nil
+		if type(check) == "table" then
+			item = check.items[1]
+		elseif instanceof(check, "InventoryItem") then
+			item = check
+		end
+
+		if item then
+			if isDebugEnabled() or (isClient() and isAdmin()) then
+				if CustomizeableLootBags.isBag(item) then
+					local optTip = context:addOptionOnTop("Set Loot Bag: "..tostring(item:getDisplayName()), worldobjects, function()
+
+						CustomizeableLootBags.setLootBag(item, false)
+
+						local msg = tostring(item)..": "..tostring(item:getDisplayName())
+						pl:addLineChatElement(tostring(msg))
+						print(tostring(msg))
+					end)
+					local tip = ISInventoryPaneContextMenu.addToolTip()
+					tip:setName("Set Loot Bag: ")
+					tip.description = tostring(CustomizeableLootBags.getStrList(item))
+					local iconPath = "media/ui/MysteryLootBag_Red.png"
+					optTip.iconTexture = getTexture(iconPath)
+					tip:setTexture(iconPath)
+					optTip.toolTip = tip
+				end
+			end
+			if CustomizeableLootBags.isLootBag(item) then
+				local optTip = context:addOptionOnTop("Open Loot Bag: "..tostring(item:getDisplayName()), worldobjects, function()
+
+					CustomizeableLootBags.openLootBag(item, false)
+
+					local msg = tostring(item)..": "..tostring(item:getDisplayName())
+					pl:addLineChatElement(tostring(msg))
+					print(tostring(msg))
+				end)
+				local tip = ISInventoryPaneContextMenu.addToolTip()
+				local iconPath = "media/ui/MysteryLootBag_Blue.png"
+				tip.description = "What could be inside?"
+				tip:setName("Open Loot Bag: ")
+				optTip.iconTexture = getTexture(iconPath)
+				tip:setTexture(iconPath)
+				optTip.toolTip = tip
+			end
+		end
+	end
+end
+Events.OnFillInventoryObjectContextMenu.Remove(CustomizeableLootBags.invContext);
+Events.OnFillInventoryObjectContextMenu.Add(CustomizeableLootBags.invContext);
